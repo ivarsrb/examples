9edcd0cba719a215a14612cb3ec2e7a94ac29309
ivarsrb <ivars.rb@gmail.com>
2020-08-07 07:18:45 +0300

    Add handlers to mjpeg-streaming

--- mjpeg-streaming/main.go
+++ mjpeg-streaming/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello streaming")
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/render", render)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func render(w http.ResponseWriter, r *http.Request) {
+
 }
